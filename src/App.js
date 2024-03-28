1b95c488d57f2a972d3232f0e3ac363b0cd43b43
Suhagiya-Piyush <piyush.suhagiya.ps@gmail.com>
2024-03-28 08:30:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+import Greeting from "./Components/Greeting";
+import Style from "../src/Components/Styles/Greeting.module.css";
+import morning from './Components/Assets/morning.jpg';
+import './Components/Styles/Style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='flex justify-center my-auto'>
+        <img src={morning} alt="" className={Style.img}/>
+        <Greeting />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Greeting.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const  Greeting =() =>{
+    let date = new Date();
+    let hours = date.getHours();
+    let Greet = "";
+    let clr;
+
+    if(hours >= 1 && hours < 12){
+        Greet = "Good Morning";
+        clr = {color : "green"};
+    }else if(hours >= 12 && hours <17){
+        Greet = "Good Afternoon!";
+        clr = {color : "blue"}
+    }else if(hours >= 17 && hours <20){
+        Greet = "Good Evening!";
+        clr = {color : "orange"}
+    }else{
+        Greet = "Good Night!";
+    }
+  return (
+    <div>
+            <h1 className="text-center text-xl font-semibold text-yellow-500 mt-10 border border-8 border-gray-500 outline-double px-10 py-5 rounded-2xl absolute top-28 left-60">Hello Komal, <span className="text-red-600" style={clr}>{Greet}</span></h1>
+        </div>
+  )
+}
+
+export default Greeting
