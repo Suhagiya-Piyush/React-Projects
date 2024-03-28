75c86752f9d967ae3f1c70404685eac90021d746
Suhagiya-Piyush <piyush.suhagiya.ps@gmail.com>
2024-03-28 09:54:09 +0530

    28-3-24

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
-import Greeting from "./Components/Greeting";
-import Style from "../src/Components/Styles/Greeting.module.css";
-import morning from './Components/Assets/morning.jpg';
-import './Components/Styles/Style.css';
+// import Greeting from "./Components/Greeting";
+// import Style from "../src/Components/Styles/Greeting.module.css";
+// import morning from './Components/Assets/morning.jpg';
+// import './Components/Styles/Style.css';
 
 function App() {
   return (
     <div className="App">
-      <div className='flex justify-center my-auto'>
+      {/* <div className='flex justify-center my-auto'>
         <img src={morning} alt="" className={Style.img}/>
         <Greeting />
-      </div>
+      </div> */}
     </div>
   );
 }
