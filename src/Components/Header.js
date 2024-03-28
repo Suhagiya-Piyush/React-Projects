6fee1bf5be335ab71929c5a5e505c0638d9e5995
Suhagiya-Piyush <piyush.suhagiya.ps@gmail.com>
2024-03-28 18:09:20 +0530

    28-3

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="bg-gray-400 py-4 hover:text-yellow-500 cursor-pointer">
+            <h1 className="text-xl font-bold">Responsive Sliding Cards in HTML CSS JS</h1>
+        </div>
+    )
+}
+
+export default Header;
