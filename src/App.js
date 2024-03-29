199aead904e27ac7c835443d4513b5e5285c304f
Suhagiya-Piyush <piyush.suhagiya.ps@gmail.com>
2024-03-29 16:19:14 +0530

    Props_Dynamic_Data_Passing

--- src/App.js
+++ src/App.js
@@ -6,12 +6,8 @@ import './App.css';
 // import './Components/Styles/Style.css';
 import Header from './Components/Header';
 import Props from './Components/Props';
-import logo_1 from './Components/Assets/Html-Logo.png';
-import logo_2 from './Components/Assets/CSS3-logo.png';
-import logo_3 from './Components/Assets/Tailwind-logo.png';
-import logo_4 from './Components/Assets/JavaScript-Logo.png';
-import logo_5 from './Components/Assets/React-logo.png';
-import logo_6 from './Components/Assets/NodeJS-Logo.png';
+import Data from './Components/Props-Data';
+
 
 function App() {
   return (
@@ -22,12 +18,16 @@ function App() {
         <Greeting />
       </div> */}
       <div className='flex flex-wrap justify-around'>
-        <Props name="* html 5 *" img={logo_1} disc="HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser."/>
+        {Data.map((cValue, ind, array) =>{
+          console.log(cValue,"cValue");
+          return (<Props key={ind} name={cValue.name} img={cValue.img} disc={cValue.disc}/>);
+        })}
+        {/* <Props name="* html 5 *" img={logo_1} disc="HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser."/>
         <Props name="* css *" img={logo_2} disc="Cascading Style Sheets (CSS) is a style sheet language used for specifying the presentation and styling of a document written in a markup language."/>
         <Props name="* Tailwind *" img={logo_3} disc="Tailwind CSS is an open source CSS framework. The main feature of this library is that, unlike other CSS frameworks like Bootstrap."/>
         <Props name="* JavaScript *" img={logo_4} disc="JavaScript, often abbreviated as JS, is a programming language and core technology of the Web, alongside HTML and CSS."/>
         <Props name="* React JS *" img={logo_5} disc="React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library for building user interfaces based on components."/>
-        <Props name="* Node JS *" img={logo_6} disc="Node.js is a cross-platform, open-source JavaScript runtime environment that can run on Windows, Linux, Unix, macOS, and more."/>
+        <Props name="* Node JS *" img={logo_6} disc="Node.js is a cross-platform, open-source JavaScript runtime environment that can run on Windows, Linux, Unix, macOS, and more."/> */}
       </div>
     </div>
   );
---
+++ src/Components/Props-Data.js
@@ -0,0 +1,42 @@
+// import React from 'react';
+import logo_1 from './Assets/Html-Logo.png';
+import logo_2 from './Assets/CSS3-logo.png';
+import logo_3 from './Assets/Tailwind-logo.png';
+import logo_4 from './Assets/JavaScript-Logo.png';
+import logo_5 from './Assets/React-logo.png';
+import logo_6 from './Assets/NodeJS-Logo.png';
+
+const Data = [
+    {
+        name : "* html 5 *",
+        img : logo_1,
+        disc : "HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser."
+    },
+    {
+        name : "* css *",
+        img : logo_2,
+        disc : "Cascading Style Sheets (CSS) is a style sheet language used for specifying the presentation and styling of a document written in a markup language."
+    },
+    {
+        name : "* Tailwind *",
+        img : logo_3,
+        disc : "Tailwind CSS is an open source CSS framework. The main feature of this library is that, unlike other CSS frameworks like Bootstrap."
+    },
+    {
+        name : "* JavaScript *",
+        img : logo_4,
+        disc : "JavaScript, often abbreviated as JS, is a programming language and core technology of the Web, alongside HTML and CSS."
+    },
+    {
+        name : "* React JS *",
+        img : logo_5,
+        disc : "React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library for building user interfaces based on components."
+    },
+    {
+        name : "* Node JS *",
+        img : logo_6,
+        disc : "Node.js is a cross-platform, open-source JavaScript runtime environment that can run on Windows, Linux, Unix, macOS, and more."
+    },
+]
+
+export default Data;
